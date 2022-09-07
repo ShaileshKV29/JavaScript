5d502f92d37151ce91c02f65347584c1b1bcca46
ShaileshKV29 <shailesh29062001@gmail.com>
2022-09-08 00:42:47 +0530

    Item Add

--- todo.js
+++ todo.js
@@ -1,2 +1,22 @@
-// 
-// 
+let item_count = 0;
+
+function addItem()
+{
+    let data = document.getElementById("todo_input").value;
+    data = data.trim();
+    if(data.length == 0)
+        alert("Please enter an Item in the Field")
+    else
+    {
+        item_count++;
+        let list_id = "item-" + item_count;
+        let item = document.getElementById("list_ul");
+        item.innerHTML += `<li id="${list_id}">${data}<button value="${list_id}">X</button></li>`;
+        document.getElementById("todo_input").value = "";
+    }
+}
+
+function deleteItem()
+{
+
+}
