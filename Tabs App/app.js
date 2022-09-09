104556d2717dd0166704fd114c8f791545a50ea6
ShaileshKV29 <shailesh29062001@gmail.com>
2022-09-10 02:16:04 +0530

    App Completed

---
+++ Tabs App/app.js
@@ -0,0 +1,18 @@
+function tab_click(element)
+{
+    const lastIndex = element.id[element.id.length-1]
+    let tab = document.querySelectorAll(".tab")
+    tab.forEach(function(node){
+        if(node.id[node.id.length-1] == lastIndex)
+        {
+            node.classList.remove("display-none")
+            node.style.display = "block";
+        }
+        else{
+            node.className = "display-none tab"
+        }
+        // console.log(typeof(node.id[node.id.length-1]))
+        // console.log(typeof(lastIndex))
+    })
+
+}
